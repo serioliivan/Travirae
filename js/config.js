3a1193838848792bd2e8ae63d89e2395f23cf4d5
serioliivan <serioliivan@gmail.com>
2025-09-12 17:52:24 +0200

    Delete js directory

--- js/config.js
+++
@@ -1,8 +0,0 @@
-// Config front-end
-export const CONFIG = {
-  PROXY_BASE_URL: 'https://travirae-proxy.vercel.app',
-  AFFILIATE_MARKER: '669407',
-  DEFAULT_CURRENCY: 'EUR',
-  DEFAULT_LOCALE: 'it',
-  DEFAULT_MARKET: 'it'
-};
