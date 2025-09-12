ed495d699552021c91711dac188749f16c4f0e77
serioliivan <serioliivan@gmail.com>
2025-09-12 16:42:23 +0200

    Delete js directory

--- js/config.js
+++
@@ -1,7 +0,0 @@
-export const CONFIG = {
-  PROXY_BASE_URL: 'https://YOUR-VERCEL-PROJECT.vercel.app', // <-- Sostituisci con il tuo dominio Vercel (senza lo slash finale)
-  AFFILIATE_MARKER: '669407',
-  DEFAULT_CURRENCY: 'EUR',
-  DEFAULT_LOCALE: 'it',
-  DEFAULT_MARKET: 'it'
-};
