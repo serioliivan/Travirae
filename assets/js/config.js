4ec9a8847e50f1cfb8a90c2ae0da5706ce5e21f8
serioliivan <serioliivan@gmail.com>
2025-09-17 22:06:43 +0200

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,9 +0,0 @@
-window.TRAVIRAE_CONFIG = {
-  "SITE_NAME": "Travirae",
-  "STAY22_AID": "travirae",
-  "STAY22_EMBED_ID": "68bed7d007d38cf140108e0d",
-  "TP_MARKER": "669407",
-  "TP_TRS": "454819",
-  "HERO_IMAGE_REMOTE": "https://images.unsplash.com/photo-1469474968028-56623f02e42e?auto=format&fit=crop&w=1600&h=900&q=60",
-  "HERO_IMAGE_LOCAL": "assets/img/hero-fallback.jpg"
-};
